a9aeb89552e34b41a05808f0bc912b1a224bfcb5
susuhaohaki <nguyenhai250203@gmail.com>
2024-08-24 07:32:48 +0700

    update banner and brand

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       boxShadow: {
         custom: "rgba(45, 74, 170, 0.08) 0px 8px 24px 0px",
+        solid_l: "0px 10px 120px 0px rgba(45, 74, 170, 0.1)",
       },
     },
   },
