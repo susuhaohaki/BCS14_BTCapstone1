510b5617ecbf6d20c949f240e0b287f5b5643c1b
susuhaohaki <nguyenhai250203@gmail.com>
2024-08-25 17:10:11 +0700

    update tabs

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,123 @@ window.addEventListener("scroll", function () {
     header.classList.add("py-7");
   }
 });
+
+let elementTabItem1 = document.getElementById("tabItem1");
+let elementTabItem2 = document.getElementById("tabItem2");
+let elementTabItem3 = document.getElementById("tabItem3");
+
+elementTabItem1.onclick = function () {
+  elementTabItem2.classList.remove(
+    "w-full",
+    "relative",
+    "before:absolute",
+    "before:bottom-0",
+    "before:left-0",
+    "before:h-1",
+    "before:w-full",
+    "before:rounded-tl-[4px]",
+    "before:rounded-tr-[4px]",
+    "before:bg-primary"
+  );
+  elementTabItem3.classList.remove(
+    "w-full",
+    "relative",
+    "before:absolute",
+    "before:bottom-0",
+    "before:left-0",
+    "before:h-1",
+    "before:w-full",
+    "before:rounded-tl-[4px]",
+    "before:rounded-tr-[4px]",
+    "before:bg-primary"
+  );
+  elementTabItem1.classList.add(
+    "w-full",
+    "relative",
+    "before:absolute",
+    "before:bottom-0",
+    "before:left-0",
+    "before:h-1",
+    "before:w-full",
+    "before:rounded-tl-[4px]",
+    "before:rounded-tr-[4px]",
+    "before:bg-primary"
+  );
+};
+
+elementTabItem2.onclick = function () {
+  elementTabItem1.classList.remove(
+    "w-full",
+    "relative",
+    "before:absolute",
+    "before:bottom-0",
+    "before:left-0",
+    "before:h-1",
+    "before:w-full",
+    "before:rounded-tl-[4px]",
+    "before:rounded-tr-[4px]",
+    "before:bg-primary"
+  );
+  elementTabItem3.classList.remove(
+    "w-full",
+    "relative",
+    "before:absolute",
+    "before:bottom-0",
+    "before:left-0",
+    "before:h-1",
+    "before:w-full",
+    "before:rounded-tl-[4px]",
+    "before:rounded-tr-[4px]",
+    "before:bg-primary"
+  );
+  elementTabItem2.classList.add(
+    "w-full",
+    "relative",
+    "before:absolute",
+    "before:bottom-0",
+    "before:left-0",
+    "before:h-1",
+    "before:w-full",
+    "before:rounded-tl-[4px]",
+    "before:rounded-tr-[4px]",
+    "before:bg-primary"
+  );
+};
+elementTabItem3.onclick = function () {
+  elementTabItem1.classList.remove(
+    "w-full",
+    "relative",
+    "before:absolute",
+    "before:bottom-0",
+    "before:left-0",
+    "before:h-1",
+    "before:w-full",
+    "before:rounded-tl-[4px]",
+    "before:rounded-tr-[4px]",
+    "before:bg-primary"
+  );
+  elementTabItem2.classList.remove(
+    "w-full",
+    "relative",
+    "before:absolute",
+    "before:bottom-0",
+    "before:left-0",
+    "before:h-1",
+    "before:w-full",
+    "before:rounded-tl-[4px]",
+    "before:rounded-tr-[4px]",
+    "before:bg-primary"
+  );
+  elementTabItem3.classList.add(
+    "w-full",
+    "relative",
+    "before:absolute",
+    "before:bottom-0",
+    "before:left-0",
+    "before:h-1",
+    "before:w-full",
+    "before:rounded-tl-[4px]",
+    "before:rounded-tr-[4px]",
+    "before:bg-primary"
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         custom: "rgba(45, 74, 170, 0.08) 0px 8px 24px 0px",
         solid_l: "0px 10px 120px 0px rgba(45, 74, 170, 0.1)",
       },
+      backgroundColor: {
+        primary: "#006bff",
+      },
     },
   },
   plugins: [require("flowbite/plugin")],
